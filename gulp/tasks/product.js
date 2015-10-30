f64a28554137c9b0860a5222549505eddd0202a5
Lukas Juhas <hello@lukasjuhas.com>
2015-10-30 14:31:21 +0000

    Refactor

--- gulp/tasks/product.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var browerSync = require('browser-sync');
-var config = require('../config');
-
-gulp.task('product', function() {
-  gulp.start(['markup-product', 'images-product', 'minify-css', 'uglify-js']);
-});
